d8545427f8fec7b9d22020f96eded12649290a74
shikou0918 <shikou.0918@gmail.com>
2023-08-10 21:13:12 +0900

    コーディング練習

--- php/test4.php
+++ php/test4.php
@@ -1,15 +1,28 @@
 <?php
 // 練習1
-$x = trim(fgets(STDIN));
+// $x = trim(fgets(STDIN));
 
-$total_days = 366;
-$lucky_days = 0;
+// $total_days = 366;
+// $lucky_days = 0;
 
-for ($day = 1; $day <= $total_days; $day++) {
-  $date_string = strval($day);
-  if (strpos($date_string, strval($X)) !== false) {
-      $lucky_days++;
-  }
+// for ($day = 1; $day <= $total_days; $day++) {
+//   $date_string = strval($day);
+//   if (strpos($date_string, strval($X)) !== false) {
+//       $lucky_days++;
+//   }
+// }
+// echo $lucky_days;
+
+// 練習2
+$input_line = trim(fgets(STDIN));
+list($n, $d) = explode(" ", $input_line);
+
+$wide = [];
+for ($i = 1; $i < $n; $i++) {
+  $first_sheet_onwards = trim(fgets(STDIN));
+  $wide[] = $d - $first_sheet_onwards;
 }
-echo $lucky_days;
+$sum = array_sum($wide) + $d;
+
+print_r($sum * $d);
 ?>
