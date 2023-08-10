d2055a0b2946b978417809e65cdc246b25993b89
shikou0918 <shikou.0918@gmail.com>
2023-08-11 08:51:31 +0900

    コーディング練習

--- php/test4.php
+++ php/test4.php
@@ -27,26 +27,47 @@
 // print_r($sum * $d);
 
 // 練習3
-$input_line = trim(fgets(STDIN));
+// $input_line = trim(fgets(STDIN));
 // 店舗数と営業期間
-list($store, $business_period) = explode(" ", $input_line);
+// list($store, $business_period) = explode(" ", $input_line);
 
 // 建設費用と人件費と利益
-$second_input_line = trim(fgets(STDIN));
-list($construction_costs, $labor_costs, $profit) = explode(" ", $second_input_line);
+// $second_input_line = trim(fgets(STDIN));
+// list($construction_costs, $labor_costs, $profit) = explode(" ", $second_input_line);
 
-$ramen_sales = [];
-for ($i = 0; $i < $store; $i++) {
-  $ramen_sales[] = trim(fgets(STDIN));
+// $ramen_sales = [];
+// for ($i = 0; $i < $store; $i++) {
+//   $ramen_sales[] = trim(fgets(STDIN));
+// }
+
+// $negative_profit_count = 0;
+// foreach ($ramen_sales as $sales) {
+//   $net_profit = $profit * $sales - $construction_costs - $labor_costs * $business_period;
+//   if ($net_profit < 0) {
+//     $negative_profit_count++;
+//   }
+// }
+
+// echo $negative_profit_count;
+
+// 練習4
+$input_line = trim(fgets(STDIN));
+$str = str_replace('-', '', $input_line);
+$telephone_number = str_split($str);
+
+$array = [12, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+$matchingValues = [];
+
+foreach ($telephone_number as $value) {
+    if (isset($array[$value])) {
+        $matchingValues[] = $array[$value];
+    }
 }
 
-$negative_profit_count = 0;
-foreach ($ramen_sales as $sales) {
-  $net_profit = $profit * $sales - $construction_costs - $labor_costs * $business_period;
-  if ($net_profit < 0) {
-    $negative_profit_count++;
-  }
-}
+$sum = array_sum($matchingValues);
 
-echo $negative_profit_count;
+echo $sum * 2;
+
+
 ?>
