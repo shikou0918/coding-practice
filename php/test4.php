ab7384d08d92df3a44a8e6aecfb80c320f4b967b
shikou0918 <shikou.0918@gmail.com>
2023-08-14 20:22:23 +0900

    コーディング練習

--- php/test4.php
+++ php/test4.php
@@ -51,23 +51,23 @@
 // echo $negative_profit_count;
 
 // 練習4
-$input_line = trim(fgets(STDIN));
-$str = str_replace('-', '', $input_line);
-$telephone_number = str_split($str);
+// $input_line = trim(fgets(STDIN));
+// $str = str_replace('-', '', $input_line);
+// $telephone_number = str_split($str);
 
-$array = [12, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// $array = [12, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-$matchingValues = [];
+// $matchingValues = [];
 
-foreach ($telephone_number as $value) {
-    if (isset($array[$value])) {
-        $matchingValues[] = $array[$value];
-    }
-}
+// foreach ($telephone_number as $value) {
+//     if (isset($array[$value])) {
+//         $matchingValues[] = $array[$value];
+//     }
+// }
 
-$sum = array_sum($matchingValues);
+// $sum = array_sum($matchingValues);
 
-echo $sum * 2;
+// echo $sum * 2;
 
 // 練習5
 // $input_line = trim(fgets(STDIN));
@@ -89,32 +89,64 @@ echo $sum * 2;
 // print_r($a_first);
 
 // 練習5
-$input_line = trim(fgets(STDIN));
-list($days, $buy, $sell) = explode(" ", $input_line);
+// $input_line = trim(fgets(STDIN));
+// list($days, $buy, $sell) = explode(" ", $input_line);
 
-$stock_prices = [];
-for ($i = 0; $i < $days; $i++) {
-    $stock_prices[] = trim(fgets(STDIN));
+// $stock_prices = [];
+// for ($i = 0; $i < $days; $i++) {
+//     $stock_prices[] = trim(fgets(STDIN));
+// }
+
+// // 持ち株
+// $total_stock = 0;
+
+// // 利益
+// $profit = 0;
+// foreach ($stock_prices as $stock_price) {
+//     if ($stock_price <= $buy) {
+//         $total_stock++;
+//         $profit -= $stock_price; // 買いの場合、損益を減算
+//     } elseif ($stock_price >= $sell) {
+//         $profit += $stock_price * $total_stock; // 売りの場合、損益を加算
+//         $total_stock = 0;
+//     }
+// }
+
+// // N 日目には持ち株を売る
+// $profit += $stock_price * $total_stock;
+
+// echo $profit;
+
+// 練習6
+$input_line = trim(fgets(STDIN));
+list($kinds, $times) = explode(" ", $input_line);
+
+$bus_times = [];
+for ($i = 0; $i < $kinds; $i++) {
+  $bus_times[] = trim(fgets(STDIN));
 }
 
-// 持ち株
-$total_stock = 0;
+$closest = [];
+foreach ($bus_times as $bus_time) {
+    $diff = abs($bus_time - $times);
+    $closest[] = array($diff, $bus_time);
+}
+sort($closest);
 
-// 利益
-$profit = 0;
-foreach ($stock_prices as $stock_price) {
-    if ($stock_price <= $buy) {
-        $total_stock++;
-        $profit -= $stock_price; // 買いの場合、損益を減算
-    } elseif ($stock_price >= $sell) {
-        $profit += $stock_price * $total_stock; // 売りの場合、損益を加算
-        $total_stock = 0;
+$minDiff = min(array_column($closest, 0));
+
+$closestBusesWithMinDiff = array();
+foreach ($closest as $bus) {
+    if ($bus[0] === $minDiff) {
+        $closestBusesWithMinDiff[] = $bus;
     }
 }
 
-// N 日目には持ち株を売る
-$profit += $stock_price * $total_stock;
+$minTimeBuses = array_column($closestBusesWithMinDiff, 1);
 
-echo $profit;
+foreach ($minTimeBuses as $minTimeBus) {
+  echo $minTimeBus . "\n";
+}
+
 
 ?>
