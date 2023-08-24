7509a18bb80c3886416e2a027174ec9d1b5e3743
shikou0918 <shikou.0918@gmail.com>
2023-08-24 21:20:38 +0900

    コーディング練習

--- php/test5.php
+++ php/test5.php
@@ -34,51 +34,69 @@
 
 // echo $pass . "\n";  // 通過した受験者の数を出力
 
-// 練習2
-$people = trim(fgets(STDIN));
+// // 練習2
+// $people = trim(fgets(STDIN));
 
-// 所持しているボールの数
-$balls = [];
-for ($i = 0; $i < $people; $i++) {
-    $balls[] = trim(fgets(STDIN));
-}
+// // 所持しているボールの数
+// $balls = [];
+// for ($i = 0; $i < $people; $i++) {
+//     $balls[] = trim(fgets(STDIN));
+// }
 
-// 何回パス回しをするか
-$passes = trim(fgets(STDIN));
+// // 何回パス回しをするか
+// $passes = trim(fgets(STDIN));
 
-for ($i = 0; $i < $passes; $i++) {
-    $input_line = trim(fgets(STDIN));
-    list($from, $to, $number) = explode(" ", $input_line);
-    if ($balls[$from - 1] < $number) {
-        $balls[$to - 1] += $balls[$from - 1];
-        $balls[$from - 1] = 0;
-    } else {
-        $balls[$to - 1] += $number;
-        $balls[$from - 1] -= $number;
-    }
-}
+// for ($i = 0; $i < $passes; $i++) {
+//     $input_line = trim(fgets(STDIN));
+//     list($from, $to, $number) = explode(" ", $input_line);
+//     if ($balls[$from - 1] < $number) {
+//         $balls[$to - 1] += $balls[$from - 1];
+//         $balls[$from - 1] = 0;
+//     } else {
+//         $balls[$to - 1] += $number;
+//         $balls[$from - 1] -= $number;
+//     }
+// }
 
-foreach ($balls as $ball) {
-    echo $ball . "\n";
-}
+// foreach ($balls as $ball) {
+//     echo $ball . "\n";
+// }
 
-// 練習3
-<?php
-// 当選番号を取得
-$winningNumbers = array_map('intval', explode(' ', trim(fgets(STDIN))));
+// // 練習3
+// // 当選番号を取得
+// $winningNumbers = array_map('intval', explode(' ', trim(fgets(STDIN))));
 
-// 購入枚数 N を取得
-$N = intval(trim(fgets(STDIN)));
+// // 購入枚数 N を取得
+// $N = intval(trim(fgets(STDIN)));
 
-// 各くじについて処理
-for ($i = 0; $i < $N; $i++) {
-    // くじの数字を取得
-    $ticketNumbers = array_map('intval', explode(' ', trim(fgets(STDIN))));
+// // 各くじについて処理
+// for ($i = 0; $i < $N; $i++) {
+//     // くじの数字を取得
+//     $ticketNumbers = array_map('intval', explode(' ', trim(fgets(STDIN))));
 
-    // 当選した数字の数をカウント
-    $matchCount = count(array_intersect($winningNumbers, $ticketNumbers));
+//     // 当選した数字の数をカウント
+//     $matchCount = count(array_intersect($winningNumbers, $ticketNumbers));
 
-    // 結果を出力
-    echo $matchCount . "\n";
+//     // 結果を出力
+//     echo $matchCount . "\n";
+// }
+
+// 練習4
+$input_line = trim(fgets(STDIN));
+list($x, $y) = explode(" ", $input_line);
+$bob = array_map('intval', str_split($x));
+$alice = array_map('intval', str_split($y));
+$bob_score = array_sum($bob);
+$alice_score = array_sum($alice);
+
+$bob_ones_digit = $bob_score % 10;
+$alice_ones_digit = $alice_score % 10;
+
+if ($bob_ones_digit > $alice_ones_digit) {
+    return "Bob";
+} elseif ($bob_ones_digit < $alice_ones_digit) {
+    return "Alice";
+} else {
+    return "Draw";
 }
 ?>
