3c229406dcb4945c439304554a74823bdbc63762
shikou0918 <shikou.0918@gmail.com>
2023-09-02 10:17:55 +0900

    コーディング練習

---
+++ php/test6.php
@@ -0,0 +1,37 @@
+<?php
+// 練習1
+$input_line = trim(fgets(STDIN));
+list($students, $questions) = explode(" ", $input_line);
+
+for ($i = 0; $i < $students; $i++) {
+    $input_line = trim(fgets(STDIN));
+    list($day, $correct_answers) = explode(" ", $input_line);
+
+    $students_results[] = array(
+        'day' => $day,
+        'correct_answers' => $correct_answers
+    );
+}
+// print_r($students_data);
+$score = 100 / $questions;
+foreach ($students_results as $students_result) {
+    $total_score = $score * $students_result['correct_answers'];
+
+    if ($students_result['day'] >= 0 && $students_result['day'] < 10) {
+      $total_score = $score * $students_result['correct_answers'] * 0.8;
+      $total_score = round($total_score); // 切り捨てて合計得点を計算
+    } elseif ($students_result['day'] >= 10) {
+      $total_score = 0;
+    }
+    if ($total_score >= 80) {
+        echo "A\n";
+    } elseif ($total_score >= 70) {
+        echo "B\n";
+    } elseif ($total_score >= 60) {
+        echo "C\n";
+    } else {
+        echo "D\n";
+    }
+}
+
+?>
