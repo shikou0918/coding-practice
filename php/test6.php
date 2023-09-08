966a9fa23021639e8e64c1df5bc4a25644588777
shikou0918 <shikou.0918@gmail.com>
2023-09-08 21:28:49 +0900

    コーディング練習

--- php/test6.php
+++ php/test6.php
@@ -35,4 +35,43 @@ foreach ($students_results as $students_result) {
 }
 
 // 練習2
+$input_line = trim(fgets(STDIN));
+list($train_fare, $one_night, $days) = explode(" ", $input_line);
+
+$internship_periods = array(); // インターンシップ期間の配列を初期化
+
+for ($i = 0; $i < $days; $i++) {
+    $input_line = trim(fgets(STDIN));
+    list($first_day, $last_day) = explode(" ", $input_line);
+    $internship_period = array(
+        'first_day' => $first_day,
+        'last_day' => $last_day
+    );
+    $internship_periods[] = $internship_period; // 期間情報を配列に追加
+}
+
+$total_price = $train_fare * 2;
+
+$result = [];
+
+for ($i = 0; $i < count($internship_periods) - 1; $i++) {
+    $current_last_day = $internship_periods[$i]['last_day'];
+    $next_first_day = $internship_periods[$i + 1]['first_day'];
+
+    // 今のインターンから次のインターンまでの日数
+    $difference_day = $next_first_day - $current_last_day;
+
+    // 次のインターンまでの宿泊費
+    $total_one_night = $difference_day * $one_night;
+
+    // 一度帰宅した時の往復の交通費
+    $total_train_fare = $train_fare * 2;
+
+    // 比較して低い方を$total_priceに加算
+    $total_price += min($total_one_night, $total_train_fare);
+}
+
+// 結果を表示
+echo $total_price;
+
 ?>
