d600d2bb56a5400f06b5f1e345a947da537f0777
shikou0918 <shikou.0918@gmail.com>
2023-10-13 17:51:37 +0900

    コーディング練習

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <p>{{ number }}</p>
+    <LikeNumber :totalNumber="number" @my-click="incrementNumber"></LikeNumber>
+    <LikeNumber :totalNumber="number" @my-click="incrementNumber"></LikeNumber>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./components//LikeHeader.vue";
+import LikeHeader from "./components/LikeHeader.vue";
 
 export default {
+  data() {
+    return {
+      number: 14
+    };
+  },
   components: {
     LikeHeader
+  },
+  methods: {
+    incrementNumber(value) {
+      this.number = value;
+    }
   }
 }
 </script>
+
+<style scoped>
+div {
+  border: 1px solid blue;
+}
+</style>
