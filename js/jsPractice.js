5395a8950dc7712968d07f572dcd49e3fe6be3f8
shikou0918 <shikou.0918@gmail.com>
2024-01-06 13:01:53 +0900

    ::

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -9,11 +9,37 @@
 //     break;
 // }
 
-let num = 4;
-let rand = Math.round(Math.random() * 10);
-console.log("ループ開始");
-while(rand !== num) {
-  console.log("rand値: " + rand);
-  rand = Math.round(Math.random() * 10);
-}
-console.log("ループ終了");
+// let num = 4;
+// let rand = Math.round(Math.random() * 10);
+// console.log("ループ開始");
+// while(rand !== num) {
+//   console.log("rand値: " + rand);
+//   rand = Math.round(Math.random() * 10);
+// }
+// console.log("ループ終了");
+
+// let ans = 0;
+// let i = 1;
+// while(i <= 100) {
+//   ans += i;
+//   i++;
+// }
+
+// console.log("答え: " + ans);
+// console.log("ループ終了のi: " + i);
+
+// let ans = 0;
+// for (let i = 1; i <= 100; i++) {
+//   ans += i;
+// }
+// console.log("答え: " + ans);
+// console.log("ループ終了のi: " + i);
+
+// let ans = 0;
+// let i = 1;
+// do {
+//   ans += i;
+//   i++;
+// } while(i <= 100);
+// console.log("答え: " + ans);
+// console.log("ループ終了のi: " + i);
